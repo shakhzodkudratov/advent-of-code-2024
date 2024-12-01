f6606f193c0453381ade1dabff4b17b2f825f097
Shakhzod Kudratov <shakhzodkudratov@gmail.com>
2024-12-01 21:51:34 +0500

    day1-2

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use itertools::Itertools;
+use std::collections::HashMap;
 use std::io::Read;
 use std::str::FromStr;
 
@@ -15,7 +16,7 @@ fn main() {
     input_file
         .read_to_string(&mut input)
         .expect("Couldn't read a file");
-    day1_1(&input);
+    day1_2(&input);
 }
 
 fn day1_1(input: &str) {
@@ -31,6 +32,27 @@ fn day1_1(input: &str) {
     b.sort();
 
     let result: i32 = a.iter().zip(b.iter()).map(|(a, b)| (a - b).abs()).sum();
+
+    println!("{:?}", result);
+}
+
+fn day1_2(input: &str) {
+    let (a, b): (Vec<_>, Vec<_>) = input
+        .split("\n")
+        .filter_map(|i| {
+            i.split("   ")
+                .filter_map(|i| i32::from_str(i).ok())
+                .next_tuple()
+        })
+        .unzip();
+
+    let mut hm: HashMap<i32, i32> = HashMap::new();
+
+    b.iter().for_each(|i| {
+        hm.entry(*i).and_modify(|i| *i += 1).or_insert(1);
+    });
+
+    let result: i32 = a.iter().map(|i| i * hm.get(i).unwrap_or(&0)).sum();
 
     println!("{:?}", result);
 }
